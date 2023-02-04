8ab739457ff8a697b621327831ddb0cc0ff2657b
JohnCruz <john.cruz6725@gmail.com>
2023-02-04 15:24:35 -0600

    .

--- remSite/settings.py
+++ remSite/settings.py
@@ -37,6 +37,8 @@ ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS",
 # Application definition
 
 INSTALLED_APPS = [
+    "resume.apps.ResumeConfig",
+    "account.apps.AccountConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
